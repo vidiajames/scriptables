5b0b86870c040a5f63d0e51aeb9e4a86202e3929
Video James <lambourn.james@gmail.com>
2020-03-28 11:39:02 +1300

    Added placeholder unit test

---
+++ Tests/Editor/ScriptableTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptableTests
+{
+    [Test]
+    public void FakeTest()
+    {
+        Assert.IsTrue(true);
+    }
+}
